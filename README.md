1fd9ab7a1a68eb25da536ae1b5b485194c8b91e5
Deekshi <me@example.com>
2023-06-19 20:13:44 +0530

    comments are added
    comments are added

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Plagarism Checcker
-  plagiarism checker that calculates the similarity between pairs of text documents using the TF-IDF (Term Frequency-Inverse Document Frequency) vectorization technique and cosine similarity metric.
+Plagiarism Checker
+This is a simple Python script that checks for plagiarism among a collection of text documents using TF-IDF (Term Frequency-Inverse Document Frequency) and cosine similarity.
+
+Features
+Identifies plagiarism between text documents.
+Utilizes the TF-IDF algorithm and cosine similarity for comparison.
+Provides similarity scores between pairs of documents.
+Supports various document formats (.txt in this example).
+Prerequisites
+Make sure you have the following requirements installed:
+
+Python 3.x
+NumPy
+scikit-learn
+Usage
+Clone the repository:
+
+bash
+Copy code
+git clone https://github.com/your-username/plagiarism-checker.git
+Navigate to the project directory:
+
+bash
+Copy code
+cd plagiarism-checker
+Place your text files in the same directory.
+
+Open the plagiarism_checker.py script and modify the sample_files list if needed.
+
+Run the script:
+
+Copy code
+python plagiarism_checker.py
+The script will compare all the text documents and display the plagiarism results.
+
+Customization
+If you want to use different document formats or change the similarity calculation method, you can modify the code accordingly.
+You can also customize the behavior by tweaking the parameters of the TF-IDF vectorizer or cosine similarity function.
+Contributing
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 import os
-from numpy import vectorize 
+from numpy import vectorize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
- 
+
+# Retrieve a list of text files in the current directory
 sample_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+
+# Read the contents of each text file and store them in a list
 sample_contents = [open(File).read() for File in sample_files]
- 
+
+# Define a lambda function to vectorize the text data using TF-IDF
 vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
+
+# Define a lambda function to calculate the cosine similarity between document vectors
 similarity = lambda doc1, doc2: cosine_similarity([doc1, doc2])
- 
+
+# Vectorize the text data using the defined lambda function
 vectors = vectorize(sample_contents)
+
+# Pair each sample file with its corresponding vectorized representation
 s_vectors = list(zip(sample_files, vectors))
- 
+
+# Function to check plagiarism
 def check_plagiarism():
     results = set()
     global s_vectors
+
+    # Iterate over each sample document and its vector
     for sample_a, text_vector_a in s_vectors:
         new_vectors = s_vectors.copy()
+
+        # Find the index of the current document vector
         current_index = new_vectors.index((sample_a, text_vector_a))
+
+        # Remove the current document vector from the new list
         del new_vectors[current_index]
+
+        # Compare the current document vector with all other vectors
         for sample_b, text_vector_b in new_vectors:
+            # Calculate the similarity score between the document vectors
             sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+
+            # Sort the document names to maintain consistent ordering
             sample_pair = sorted((sample_a, sample_b))
+
+            # Create a tuple with document pair and similarity score
             score = sample_pair[0], sample_pair[1], sim_score
+
+            # Add the score tuple to the results set
             results.add(score)
+
     return results
- 
+
+# Iterate over the plagiarism check results and print them
 for data in check_plagiarism():
-    print(data)    
+    print(data)
+  
